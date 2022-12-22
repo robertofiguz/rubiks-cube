4c98bda832f89a94ac6ba0269b97835e614949a6
SousaUA <hsousa@ua.pt>
2022-12-22 12:44:23 -0100

    First approach to detect colors --needs improvement

--- src/detect-cube-face.py
+++ src/detect-cube-face.py
@@ -2,8 +2,11 @@ import cv2
 import sys
 import numpy as np
 from imutils import contours
+from collections import namedtuple
 
 SQUARE_THRESHOLD = 150
+FACE_WINDOW = "face"
+WEBCAM_WINDOW = "camera"
 
 def detect_cube_face(camera):
     ''' Detects the cube face and square colors
@@ -19,6 +22,8 @@ def detect_cube_face(camera):
         contours, hierarchy = cv2.findContours(binary,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
     
     squares = []
+    square_images = []
+    square_colors = []
     for contour in contours:
         area = cv2.contourArea(contour)
 
@@ -40,14 +45,69 @@ def detect_cube_face(camera):
     else:
         face_detected = True
         cv2.putText(camera, "Face detected", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-    for square in squares:
-        cv2.drawContours(camera, [square], 0, (0, 255, 0), 2)
-
+    
     face = camera if face_detected else None
+    
+    SQUARE = namedtuple('SQUARE', ['image','x', 'y', 'w', 'h'])
+    for square in squares:
+        if face is not None:
+            # Detect the color of the square
+            x, y, w, h = cv2.boundingRect(square)
+            square_images.append(SQUARE(camera[y:y+h, x:x+w], x, y, w, h))
+        cv2.drawContours(camera, [square], 0, (0, 255, 0), 2)
+    
 
+    for square in square_images:
+        color = detect_square_color(square)
+        square_colors.append(color)
+        cv2.putText(camera, color, (square.x, square.y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    square_colors = square_colors[::-1]
+    
     return camera, face
 
+# TODO: This function needs improvement
+def detect_square_color(square):
+    #FIXME: Use calibrated values for the range of colors
+    hsv = cv2.cvtColor(square.image, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+    h = h.flatten()
+    s = s.flatten()
+    v = v.flatten()
+    h = h[h > 0]
+    s = s[s > 0]
+    v = v[v > 0]
+    h = np.mean(h)
+    s = np.mean(s)
+    v = np.mean(v)
+    if h < 10:
+        return 'R'
+    elif 10 < h < 25:
+        return 'O'
+    elif 25 < h < 35:
+        return 'Y'
+    elif 35 < h < 77:
+        return 'G'
+    elif 77 < h < 99:
+        return 'B'
+    elif 99 < h < 124:
+        return 'W'
+    return 'NA'
+
+def init_windows():
+    cv2.namedWindow(FACE_WINDOW)
+    cv2.moveWindow(FACE_WINDOW, 1200, 0)
+
+    cv2.namedWindow(WEBCAM_WINDOW)
+    cv2.moveWindow(WEBCAM_WINDOW, 0, 0)
+
+    face_background = np.zeros((480, 640, 3), np.uint8)
+    cv2.putText(face_background, "When a cube face is detected it will be displayed here", (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+    cv2.imshow(FACE_WINDOW, face_background)
+        
 def main():
+
+    init_windows()
+
     video = cv2.VideoCapture(0)
     is_ok, bgr_image_input = video.read()
     
@@ -62,18 +122,11 @@ def main():
 
         camera, face = detect_cube_face(camera)
 
-        # try:
-        #     camera, mask = mask_cube_face(camera)
-        # except ValueError:
-        #     print("No face detected")
-        #     continue    
 
-
-        cv2.imshow("camera", camera)
+        cv2.imshow(WEBCAM_WINDOW, camera)
         if face is not None:
-            cv2.imshow("face", face)
-        #cv2.imshow("binary", binary)
-        #cv2.imshow("mask", mask)
+            cv2.imshow(FACE_WINDOW, face)
+
         key_pressed = cv2.waitKey(1) & 0xFF
         if key_pressed == 27 or key_pressed == ord('q'):
             break
